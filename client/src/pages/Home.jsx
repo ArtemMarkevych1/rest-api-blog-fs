44853f1d3a94b0e79a0188bb40c52cadf9769a6e
amarkevych <artem@govplus.com>
2024-11-27 16:27:31 +0200

    tc

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -120,7 +120,7 @@ function Home() {
           )}
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
           {posts && posts.length > 0 && posts.map(post => (
             <PostCard
               key={post._id}
